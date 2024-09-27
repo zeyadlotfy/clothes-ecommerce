a1c8f12cc8496e00932a4698389e717fdc8feb5d
Zeyad Lotfy <zezoramy380@gmail.com>
2024-09-28 01:01:20 +0300

    S

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
     return NextResponse.redirect(
         process.env.NODE_ENV === "development"
             ? "http://localhost:3000/"
-            : "https://shoe-marshal.vercel.app/"
+            : "https://clothes-ecommerce-86yy3w8y0-zeyad-lotfys-projects.vercel.app/"
     );
 }
